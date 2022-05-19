3f715cd4b929def816bc081cd686e3f9d1eecce0
miracle3070 <miracle3070@naver.com>
2022-05-20 00:33:53 +0900

    옵션 처리

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -14,9 +14,11 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
+    /*
     // 수동으로 빈 등록. (테스트 코드는 성공하지만 스프링 부트로 실행하면 오류남.)
     @Bean(name = "memoryMemberRepository")
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+    */
 }
